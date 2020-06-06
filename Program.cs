5630a0e79a864c5b2e15643e4a090653f5e64021
diego_neri <diego@diegoneri.com>
2020-06-05 21:24:36 -0300

    Finaliza o exercício

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HoraSistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Neste momento são exatamente {DateTime.Now}");
+        }
+    }
+}
